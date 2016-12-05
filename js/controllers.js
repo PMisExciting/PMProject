205a2db95d2ac3687c006ae5733ff30d5bd84ddb
Sunny <13302010096@fudan.edu.cn>
2016-12-05 15:24:49 +0800

    论坛页面写完了233

--- js/controllers.js
+++ js/controllers.js
@@ -15,16 +15,69 @@ angular.module('myApp.controllers', [])
 
     })
 
-    .controller('forumCtrl', function($scope){
-        $scope.forums = [
-            {forumTitle:"论我为什么要起一个这样的标题名", forumContent:"测试内容1", forumTime:"2016-12-03 20:00:00", userName:"用户1", commentNum:"10", forumId:0},
-            {forumTitle:"因为它看起来肥肠好看啊哈哈哈哈哈", forumContent:"测试内容2", forumTime:"2016-12-03 20:00:00", userName:"用户2", commentNum:"100", forumId:1},
-            {forumTitle:"短小精悍", forumContent:"测试内容3", forumTime:"2016-12-03 20:00:00", userName:"用户3", commentNum:"0", forumId:2},
-            {forumTitle:"论我为什么要起一个这样的标题名", forumContent:"测试内容1", forumTime:"2016-12-03 20:00:00", userName:"用户1", commentNum:"10", forumId:0},
-            {forumTitle:"因为它看起来肥肠好看啊哈哈哈哈哈", forumContent:"测试内容2", forumTime:"2016-12-03 20:00:00", userName:"用户2", commentNum:"100", forumId:1},
-            {forumTitle:"短小精悍", forumContent:"测试内容3", forumTime:"2016-12-03 20:00:00", userName:"用户3", commentNum:"0", forumId:2},
-            {forumTitle:"短小精悍", forumContent:"测试内容3", forumTime:"2016-12-03 20:00:00", userName:"用户3", commentNum:"0", forumId:2}
-        ];
+    .controller('forumCtrl', function($scope, $http, $rootScope){
+        $http.get("http://" + $rootScope.hostUrl + ":8080/PMServer/GetForums")
+            .success(function(ret){
+                if(ret != null && ret.length != 0){
+                    $rootScope.forums = ret;
+                    $scope.forumsLength = ret.length;
+
+                    //初始显示6条帖子,每次点击"更多"额外多显示6条
+                    if(ret.length < 6){ //如果当前总数小于6,则改变按钮内容
+                        $scope.forumsPart = $rootScope.forums;
+                        document.getElementsByClassName("more-forum")[0].innerHTML = "没有更多啦";
+                    }else{ //否则只显示前6条,并记录Index
+                        $scope.forumsPart = $rootScope.forums.slice(0,6);
+                        $scope.index = 6;
+                    }
+                }else{
+                    document.getElementsByClassName("more-forum").innerHTML = "没有更多啦";
+                }
+            })
+            .error(function(){
+                alert('因网络原因无法获取信息');
+            });
+
+        $http.get("http://" + $rootScope.hostUrl + ":8080/PMServer/GetHotForums")
+            .success(function(ret){
+                if(ret != null && ret.length != 0){
+                    $scope.hotforums = ret;
+                }else{
+                    document.getElementsByClassName("null-error")[0].innerHTML = "没有新帖,快去发布吧";
+                }
+            }).error(function(){
+                alert('因网络原因无法获取信息');
+            });
+
+        if($rootScope.user){
+            $http.get("http://" + $rootScope.hostUrl + ":8080/PMServer/GetForumsByUserId",{params:{userId: $rootScope.userId}})
+                .success(function(ret){
+                    if(ret != null && ret.length != 0){
+                        $scope.myforums = ret;
+                    }else{
+                        document.getElementsByClassName("null-error")[1].innerHTML = "没有新帖,快去发布吧";
+                    }
+                }).error(function(){
+                    alert('因网络原因无法获取信息');
+                });
+        }else{
+            document.getElementsByClassName("null-error")[1].innerHTML = "<a href=\"#login\">请先登录</a>";
+        }
+
+        $scope.moreForums = function(){
+            //如果剩余的文章数大于6,则添加6条,并记录index
+            if($scope.forumsLength-$scope.forumsPart.length > 6){
+                $scope.forumsPart = $rootScope.forums.slice(0,$scope.index + 6);
+                $scope.index += 6;
+            }else{ //否则把剩余的内容全部添加到当前展示的数组中,并改变按钮内容
+                $scope.forumsPart = $rootScope.forums;
+                document.getElementsByClassName("more-forum")[0].innerHTML = "没有更多啦";
+            }
+        }
+
+        $scope.addForum = function(){
+            //TODO
+        }
     })
 
     .controller('forumDetailCtrl', function(){
@@ -47,6 +100,7 @@ angular.module('myApp.controllers', [])
                 .success(function (ret) {
                     if (ret != null && ret[0] != null) {
                         $rootScope.user = ret[0];
+                        $rootScope.userId =  $rootScope.user.userId;
                         window.location.href = "#home";
                     } else {
                         error.innerHTML = "用户名或密码不正确";
